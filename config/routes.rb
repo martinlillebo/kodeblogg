0f3d55d60883819ff7dfa03168858436e199d9e4
Martin Lillebo <martin@pqa.no>
2020-10-19 19:27:51 +0200

    headerstyling, noen knapper, litt tekst

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'sider#forside'
   # root to: 'sider#forside' <--- Dette er langformen
-  
+  get 'hva-er-kodebloggen', to: 'sider#hva-er-kodebloggen'
+  get 'artikler', to: 'sider#artikler'
 end
