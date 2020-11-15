999d4adc030811103a546127446889a929cd3723
Martin Lillebo <martin@pqa.no>
2020-11-15 17:17:58 +0100

    rammeverkartikkel v1.0 ferdig

--- app/controllers/bloggposter_controller.rb
+++ app/controllers/bloggposter_controller.rb
@@ -6,5 +6,26 @@ class BloggposterController < ApplicationController
   end
 
   def rammeverk
+    url = URI("https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json")
+    https = Net::HTTP.new(url.host, url.port);
+    https.use_ssl = true
+    request = Net::HTTP::Get.new(url)
+    response = https.request(request)
+  
+    @sykkelhash = JSON.parse(response.read_body)["data"]["stations"]
+  
+    url = URI("https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json")
+    https = Net::HTTP.new(url.host, url.port);
+    https.use_ssl = true
+    request = Net::HTTP::Get.new(url)
+    response = https.request(request)
+  
+    @stasjonsinfo = JSON.parse(response.read_body)["data"]["stations"]
+  
+    @id_hash = {}
+  
+    @stasjonsinfo.each do |element| 
+      @id_hash[element["station_id"]] = element["name"]
+    end
   end
 end
